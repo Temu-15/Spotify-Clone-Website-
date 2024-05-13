4a60bfce87a736ee53eb3efd082b739fb6c2bb7c
Temu-15 <abuadanu89@gmail.com>
2024-05-13 13:53:49 +0300

    changed the redirect uri

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -1,5 +1,5 @@
 const clientId = "2f9e3cd2c1f7476088ad528bdf162c8c";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-website-react.netlify.app";
 const apiUrl = "https://accounts.spotify.com/authorize";
 const scopes = [
   "user-read-email",
